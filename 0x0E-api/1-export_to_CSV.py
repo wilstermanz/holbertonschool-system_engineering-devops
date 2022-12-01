cf48b193bd8c666ec8f956218407bce1686c656b
wilstermanz <wilstermanz@gmail.com>
2022-11-30 20:51:16 -0600

    Solution for task 2

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script uses an api to return a TODO list using
+a given employee ID
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    with open(argv[1]+".csv", 'w') as csv_file:
+        employee_info = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}"
+            .format(argv[1])).json()
+        for k, v in employee_info.items():
+            if k == "name":
+                employee_name = v
+            if k == "username":
+                employee_username = v
+
+        todo_list = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}/todos"
+            .format(argv[1])).json()
+        tasks_complete = []
+        for task in todo_list:
+            for k, v in task.items():
+                if k == "completed" and v is True:
+                    tasks_complete.append(task["title"])
+            csv_file.write('"{}","{}","{}","{}"\n'.format(
+                argv[1], employee_username, task["completed"], task["title"]))
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            employee_name, len(tasks_complete), len(todo_list)))
+        for task in tasks_complete:
+            print("\t {}".format(task))
