ced7ec29d29247ffbe9c9cf818718f174f99053b
wilstermanz <wilstermanz@gmail.com>
2022-11-30 21:09:13 -0600

    Solution for task 2

---
+++ 0x0E-api/2-export_to_JSON.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This script uses an api to return a TODO list using
+a given employee ID
+"""
+import requests
+import json
+from sys import argv
+
+
+if __name__ == "__main__":
+    employee_info = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(argv[1])).json()
+    for k, v in employee_info.items():
+        if k == "name":
+            employee_name = v
+        if k == "username":
+            employee_username = v
+
+    todo_list = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos"
+        .format(argv[1])).json()
+    tasks_complete = []
+    json_lib = {argv[1]: []}
+    task_list = json_lib[argv[1]]
+    for task in todo_list:
+        for k, v in task.items():
+            if k == "completed" and v is True:
+                tasks_complete.append(task["title"])
+        task_list.append(
+            {"task": task["title"],
+             "completed": task["completed"],
+             "username": employee_username})
+
+    with open(argv[1]+".json", 'w') as json_file:
+        json.dump(json_lib, json_file)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(tasks_complete), len(todo_list)))
+    for task in tasks_complete:
+        print("\t {}".format(task))
